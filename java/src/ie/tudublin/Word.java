6689a06b0653796896df0e7432011c363db89967
emilroy4 <emilroy100@gmail.com>
2023-04-26 18:08:46 +0100

    comment

--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -4,10 +4,11 @@ package ie.tudublin;
 
 import java.util.ArrayList;
 
-public class Word {
+public class Word {//represents a word in the model that is used to generate sonnets
     private String word;
     private ArrayList<Follow> follows;
 
+    //constuctor
     public Word(String word) {
         this.word = word;
         follows = new ArrayList<>();
@@ -20,17 +21,17 @@ public class Word {
     public ArrayList<Follow> getFollows() {
         return follows;
     }
-
+    //sets the word to the string that is used to generate sonnets
     public void SetWord(String word) {
         this.word = word;
     }
-
+//adds a follow object to the arraylist
     public void addFollow(Follow follow) {
         follows.add(follow);
     }
-
+    //a method that returns null if there is no match
     public Follow findFollow(String str) {
-        for (Follow follow : follows) {
+        for (Follow follow : follows) { 
             if (follow.getWord().equals(str)) {
                 return follow;
             }
